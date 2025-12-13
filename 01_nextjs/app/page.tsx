fbaa7b5d1954ebf49d113529d26d876f56d1330b
Soma <jadhavsomnath7005@gmail.com>
2025-12-13 20:37:59 +0530

    first commit

---
+++ 01_nextjs/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+   <div className="flex flex-col h-screen justify-center items-center">
+    <h1 className="text-4xl text-green-500 font-bold">Hello World</h1>
+   </div>
+  );
+}
