69861b08868f9ae70f67138dfffe6141c38dda5a
Soma <jadhavsomnath7005@gmail.com>
2025-12-14 21:49:31 +0530

    add:  03_mini_project_01 based on routing

---
+++ 03_mini_project_01/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-50 border-t border-gray-200'>
+      <div className='max-w-6xl mx-auto px-4 py-8 merquee'>
+        <p className='text-gray-600 text-sm'>@2025 MYWEBSITE. All Rights Reserved</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
