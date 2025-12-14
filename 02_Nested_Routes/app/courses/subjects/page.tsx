b8114f5a978e10f267854c592d67e1a03405b975
Soma <jadhavsomnath7005@gmail.com>
2025-12-14 21:48:28 +0530

    add:  02_Nested_Routes for routes understanding

---
+++ 02_Nested_Routes/app/courses/subjects/page.tsx
@@ -0,0 +1,17 @@
+
+
+const Subjects = () => {
+  return (
+    <div>Subjects
+        <ul>
+            <li>hi</li>
+            <li></li>
+            <li></li>
+            <li>mama</li>
+            <li></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Subjects
---
+++ 02_Nested_Routes/app/products/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsId = ({params}) => {
+  return (
+    <div>ProductsId id {params.id}</div>
+  )
+}
+
+export default ProductsId
