abfb6002d052017ab83d40453f9e63b74d79b462
Soma <jadhavsomnath7005@gmail.com>
2025-12-25 19:37:52 +0530

    add: searchParams hook in nextjs

--- 08_Hooks/app/search/page.js
+++ 08_Hooks/app/search/page.js
@@ -6,8 +6,9 @@ const SearchPage = () => {
     const searchParams = useSearchParams()
     console.log(searchParams)
     const query = searchParams.get("name")
+    const allParams = Array.from(searchParams.entries())
   return (
-    <div><h1>{query}</h1></div>
+    <div><h1>{allParams}</h1></div>
   )
 }
 
