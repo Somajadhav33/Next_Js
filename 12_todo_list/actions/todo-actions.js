8facf588f7c3a044119805fe0eee5928a04226eb
Soma <jadhavsomnath7005@gmail.com>
2026-02-11 22:10:29 +0530

    upgrade validations

--- 12_todo_list/actions/todo-actions.js
+++ 12_todo_list/actions/todo-actions.js
@@ -3,3 +3,13 @@
 import { revalidatePath } from "next/cache";
 import connectDb from "@/lib/db";
 import TodoSchema from "@/model/todo";
+import { createTodoSchema } from "@/validations/todo";
+
+export async function CreateTodo(data) {
+  try {
+    const validateData = createTodoSchema.parse(data);
+    await connectDb();
+    const todo = await TodoSchema.create(validateData);
+    revalidatePath("/");
+  } catch (e) {}
+}
--- 12_todo_list/validations/todo.js
+++ 12_todo_list/validations/todo.js
@@ -1,3 +1,11 @@
 import { z } from "zod";
 
-export const createTodo;
+export const createTodoSchema = z.object({
+  tital: z
+    .string()
+    .min(1, "Title is required")
+    .max(100, "title must be less than hundred characters")
+    .trim(),
+  description: z.string().max(500, "Description must be < 500").optional(),
+  priority: z.enum(["low", "medium", "high"]).default("medium"),
+});
