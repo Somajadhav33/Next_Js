31a6384161971b627fed832fc05e2d54c487b846
Soma <jadhavsomnath7005@gmail.com>
2026-03-12 10:46:00 +0530

    clerk-auth

---
+++ 14_authorization_authentication/clerk-auth/app/page.js
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+
+export default async function Home() {
+  const user = await currentUser();
+  console.log(user);
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <UserButton />
+    </div>
+  );
+}
