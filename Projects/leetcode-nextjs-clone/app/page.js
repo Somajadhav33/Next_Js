57e679d64499e4f7c3e621abe9b41fc05625471f
Soma <jadhavsomnath7005@gmail.com>
2026-03-16 14:00:07 +0530

    cleark-auth setup

--- Projects/leetcode-nextjs-clone/app/page.js
+++ Projects/leetcode-nextjs-clone/app/page.js
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
-
-import Image from "next/image";
+import { SignOutButton, SignInButton, UserButton, Show } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click Me</Button>
+      <UserButton />
     </div>
   );
 }
