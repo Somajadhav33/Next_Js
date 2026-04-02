ed23537b5c6dc81a5a5e4e65ca233da25f4cbaeb
Soma <jadhavsomnath7005@gmail.com>
2026-04-02 12:17:28 +0530

    add to playlist feature added

--- Projects/leetcode-nextjs-clone/modules/problems/components/problem-table.jsx
+++ Projects/leetcode-nextjs-clone/modules/problems/components/problem-table.jsx
@@ -39,6 +39,8 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { Checkbox } from "@/components/ui/checkbox";
 import { toast } from "sonner";
+import CreatePlaylistModal from "./create-playlist";
+import AddToPlaylistModal from "./add-to-playlist";
 
 const ProblemsTable = ({ problems, user }) => {
   const [search, setSearch] = useState("");
@@ -378,6 +380,19 @@ const ProblemsTable = ({ problems, user }) => {
           </div>
         </div>
       )}
+      {/* Modals */}
+      <CreatePlaylistModal
+        isOpen={isCreateModalOpen}
+        onClose={() => setIsCreateModalOpen(false)}
+        onSubmit={handleCreatePlaylist}
+      />
+
+      <AddToPlaylistModal
+        isOpen={isAddToPlaylistModalOpen}
+        onClose={() => setIsAddToPlaylistModalOpen(false)}
+        onSubmit={handleAddToPlaylist}
+        problemId={selectedProblemId}
+      />
     </div>
   );
 };
