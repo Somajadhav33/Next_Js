acd3d5f3dd2474842936e0d0057860043e805010
Soma <jadhavsomnath7005@gmail.com>
2026-04-11 07:43:40 +0530

    inngest AI agent setup

--- Projects/v0-clone/src/inngest/functions.js
+++ Projects/v0-clone/src/inngest/functions.js
@@ -1,15 +1,22 @@
 // src/inngest/functions.ts
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const processTask = inngest.createFunction(
-  { id: "process-task", triggers: { event: "app/task.created" } },
+  { id: "agent/mewowowow", triggers: { event: "app/task.created" } },
   async ({ event, step }) => {
-    const result = await step.run("handle-task", async () => {
-      return { processed: true, id: event.data.id };
+    const sampleAgent = createAgent({
+      name: "sample-agent",
+      description: "Sample Agent to demonstrate",
+      system: "You are helpful assistant",
+      model: gemini({ model: "gemini-2.5-flash" }),
     });
 
-    await step.sleep("pause", "1s");
-
-    return { message: `Task complete`, result };
+    const { output } = await sampleAgent.run("What is Next.js ??");
+    return {
+      message: output[0].content,
+    };
   },
 );
+
+const model = gemini({ model: "gemini-1.5-flash" });
